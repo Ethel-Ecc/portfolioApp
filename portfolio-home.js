724cd7e1893bc4be213ae6a4317e3114d1ae12a2
Ethel <ethelbert@ethel-consulting.com>
2017-08-09 15:26:34 +0200

    Create portfolio-home.js

---
+++ portfolio-home.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Ethel_007 on 9/20/2016.
+ */
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
+
+$('.nav a').on('click', function(){
+    $('.nav').find('.active').removeClass("active");
+    $(this).parent().addClass("active");
+});
